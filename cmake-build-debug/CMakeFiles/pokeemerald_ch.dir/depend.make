f534629f828f820be90934797d512c266c3e15cc
root <root@localhost>
2023-08-23 20:50:49 +0800

    pokeemerald-chinese

---
+++ cmake-build-debug/CMakeFiles/pokeemerald_ch.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pokeemerald_ch.
+# This may be replaced when dependencies are built.
---
+++ data/unknown_serial_data.s
@@ -0,0 +1,14 @@
+	.section .rodata
+
+	.align 2
+gUnknown_089A3470:: @ 89A3470
+	.incbin "data/unknown_serial_data.bin"
+
+    .align 2
+gFont1Chinese::
+    .incbin "graphics/fonts/font1_ch.bin"
+    //.incbin "graphics/fonts/fr1.bin",0,0x6BDC0
+    .align 2
+gFont0Chinese::
+    .incbin "graphics/fonts/font0_ch.bin"
+    //.incbin "graphics/fonts/fr1.bin",0x80000
